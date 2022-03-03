f19e7f011ad3789f71c1d3a06944c874f0508dd6
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-03 23:46:00 +0200

    added product controller

---
+++ LDBeauty/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LDBeauty.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
