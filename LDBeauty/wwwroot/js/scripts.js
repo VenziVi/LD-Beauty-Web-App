fbba598ca987ce3e8322bcce00bfd66a5cf5b8de
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-05 23:42:21 +0200

    added static img

--- LDBeauty/wwwroot/js/scripts.js
+++ LDBeauty/wwwroot/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Heroic Features v5.0.4 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
