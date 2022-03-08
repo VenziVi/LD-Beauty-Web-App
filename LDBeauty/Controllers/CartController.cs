2cc42934d2a66e186f83a952b1a629746420e4a2
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-08 21:56:35 +0200

    added cart controller

---
+++ LDBeauty/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LDBeauty.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
