4217af1e2189cbe45f472c453eb98dab0a99a99f
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-08 21:59:39 +0200

    implement service view

---
+++ LDBeauty/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LDBeauty.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
