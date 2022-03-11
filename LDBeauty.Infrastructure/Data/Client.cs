4fe72b918f6926392d4d9489ea57264271d1f43f
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-11 22:50:03 +0200

    change client data model

--- LDBeauty.Infrastructure/Data/Client.cs
+++ LDBeauty.Infrastructure/Data/Client.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LDBeauty.Infrastructure.Data
+﻿namespace LDBeauty.Infrastructure.Data
 {
     public class Client
     {
