9936b5e4e0d649c274c7d18a0ef611cc5c7cddb6
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-12 14:32:57 +0200

    added migrations

---
+++ LDBeauty.Infrastructure/Migrations/20220312115509_addedPriceRange.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LDBeauty.Infrastructure.Migrations
+{
+    public partial class addedPriceRange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
