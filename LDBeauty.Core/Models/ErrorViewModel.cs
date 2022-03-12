286cb64467aeed080b8df7dd4b77e633d5cd8ec6
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-12 18:14:14 +0200

    add gallery to view models

---
+++ LDBeauty.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LDBeauty.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+    }
+}
---
+++ LDBeauty.Core/Models/Gallery/AddImageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LDBeauty.Core.Models
+{
+    public class AddImageViewModel
+    {
+        [Required]
+        public string PictureUrl { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
