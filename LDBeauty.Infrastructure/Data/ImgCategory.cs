855f039fbb755e2ba9606aa066ea3f088f56d50c
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-12 18:13:10 +0200

    added imgUrl to ImgCategory model

--- LDBeauty.Infrastructure/Data/ImgCategory.cs
+++ LDBeauty.Infrastructure/Data/ImgCategory.cs
@@ -11,6 +11,9 @@ namespace LDBeauty.Infrastructure.Data
         [StringLength(20)]
         public string CategoryName { get; set; }
 
+        [Required]
+        public string ImgUrl { get; set; }
+
         public IList<Image> Images { get; set; } = new List<Image>();
     }
 }
---
+++ LDBeauty.Infrastructure/Migrations/20220312155424_add img to ImgCategories.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LDBeauty.Infrastructure.Migrations
+{
+    public partial class addimgtoImgCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "ImgCategories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "ImgCategories");
+        }
+    }
+}
