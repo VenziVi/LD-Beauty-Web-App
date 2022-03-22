e1a565cbf41b190a8a23fb86212486b0262549e3
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-23 00:01:20 +0200

    added admin controllers

---
+++ LDBeauty/Areas/Admin/Controllers/ImageController.cs
@@ -0,0 +1,48 @@
+﻿using LDBeauty.Core.Constants;
+using LDBeauty.Core.Contracts;
+using LDBeauty.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LDBeauty.Areas.Admin.Controllers
+{
+    public class ImageController : BaseController
+    {
+
+        private readonly IGalleryService galleryService;
+
+        public ImageController(IGalleryService _galleryService)
+        {
+            galleryService = _galleryService;
+        }
+
+        public IActionResult AddImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddImage(AddImageViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData[MessageConstant.ErrorMessage] = "Data is not correct!";
+                return View();
+            }
+
+            try
+            {
+                await galleryService.AddImage(model);
+            }
+            catch (Exception)
+            {
+
+                ViewData[MessageConstant.ErrorMessage] = "Something went wrong!";
+                return View();
+            }
+
+           
+            ViewData[MessageConstant.SuccessMessage] = "Data have been saved!";
+            return View();
+        }
+    }
+}
---
+++ LDBeauty/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LDBeauty.Areas.Admin.Controllers
+{
+    public class ProductController : BaseController
+    {
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
