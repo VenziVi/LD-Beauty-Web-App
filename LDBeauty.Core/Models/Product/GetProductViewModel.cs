120f91fda39473dcc00f721e2d18674736abc1c2
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-25 23:06:30 +0200

    added product view model

---
+++ LDBeauty.Core/Models/Product/GetProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Models.Product
+{
+    public class GetProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Make { get; set; }
+    }
+}
---
+++ LDBeauty.Core/Models/Product/ProductDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Models.Product
+{
+    public class ProductDetailsViewModel : AddProductViewModel
+    {
+        public Guid Id { get; set; }
+    }
+}
