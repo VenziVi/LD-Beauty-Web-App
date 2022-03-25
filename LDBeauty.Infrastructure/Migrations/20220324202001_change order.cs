add871bc51695b302b8da2891c810ac2ca75c381
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-25 23:05:38 +0200

    added migration

---
+++ LDBeauty.Infrastructure/Migrations/20220324202001_change order.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LDBeauty.Infrastructure.Migrations
+{
+    public partial class changeorder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Carts_CartId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CartId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "Carts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderId",
+                table: "AddedProducts",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AddedProducts_OrderId",
+                table: "AddedProducts",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AddedProducts_Orders_OrderId",
+                table: "AddedProducts",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AddedProducts_Orders_OrderId",
+                table: "AddedProducts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AddedProducts_OrderId",
+                table: "AddedProducts");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "AddedProducts");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CartId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CartId",
+                table: "Orders",
+                column: "CartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Carts_CartId",
+                table: "Orders",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
