7243675e3514e6b805ad355279f907bcc2b765d3
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-26 23:01:40 +0200

    added UserOrderViewModel

---
+++ LDBeauty.Core/Models/User/UserOrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Models.User
+{
+    public class UserOrderViewModel
+    {
+        public string Id { get; set; }
+
+        public string CartId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
