372cd2339795f9913f9a3964749dc72dca4f56d9
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-26 23:01:56 +0200

    added cart view models

--- LDBeauty.Core/Models/Cart/AddToCartViewModel.cs
+++ LDBeauty.Core/Models/Cart/AddToCartViewModel.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LDBeauty.Core.Models.Cart
+﻿namespace LDBeauty.Core.Models.Cart
 {
     public class AddToCartViewModel
     {
         public Guid ProductId { get; set; }
-
         public int Quantity { get; set; }
+
     }
 }
---
+++ LDBeauty.Core/Models/Cart/CartDetailsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Models.Cart
+{
+    public class CartDetailsViewModel
+    {
+        public Guid CartId { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public List<CartProductsViewModel> Products { get; set; }
+    }
+
+    public class CartProductsViewModel
+    {
+        public string ProductName { get; set; }
+
+        public string ProductMake { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Price { get; set; }
+    }
+}
