e67c3670f03c90a016fae1ebf32cb7ba55daef3c
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-27 23:37:15 +0300

    added migraton remove phone validation

---
+++ LDBeauty.Infrastructure/Migrations/20220327165704_remove phon validation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LDBeauty.Infrastructure.Migrations
+{
+    public partial class removephonvalidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
