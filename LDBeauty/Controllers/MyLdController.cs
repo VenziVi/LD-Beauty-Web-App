2b36e05a434504fbca37dc76b5400a9c115326bc
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-29 00:10:06 +0300

    implement myLdController

--- LDBeauty/Controllers/MyLdController.cs
+++ LDBeauty/Controllers/MyLdController.cs
@@ -1,17 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LDBeauty.Core.Contracts;
+using LDBeauty.Core.Models.User;
+using LDBeauty.Core.Services;
+using LDBeauty.Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LDBeauty.Controllers
 {
     public class MyLdController : Controller
     {
-        public IActionResult Info()
-        {
-            if (!User.Identity.IsAuthenticated)
-            {
-                return Redirect("~/Identity/Account/Login");
-            }
+        private readonly IUserService userService;
+        private readonly IOrderService orderService;
 
-            return View();
+        public MyLdController(
+            IUserService _userService,
+            IOrderService _orderService)
+        {
+            userService = _userService;
+            orderService = _orderService;
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> Info()
+        {
+            var userName = User.Identity.Name;
+            ApplicationUser user = await userService.GetUser(userName);
+            ViewData["UserName"] = user.FirstName;
+
+            List<UserProductsViewModel> products = await orderService.GetUserProducts(user.Id);
+            return View(ViewData);
         }
     }
 }
