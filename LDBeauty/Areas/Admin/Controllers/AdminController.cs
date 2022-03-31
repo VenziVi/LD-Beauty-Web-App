5e8a237667640bd2d8437e15d2cb955add38dad3
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-31 23:41:45 +0300

    implement admin controller

---
+++ LDBeauty/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using LDBeauty.Core.Contracts;
+using LDBeauty.Core.Models.User;
+using LDBeauty.Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LDBeauty.Areas.Admin.Controllers
+{
+    public class AdminController : BaseController
+    {
+        private readonly IUserService userService;
+        private readonly IOrderService orderService;
+
+        public AdminController(
+            IUserService _userService,
+            IOrderService _orderService)
+        {
+            userService = _userService;
+            orderService = _orderService;
+        }
+
+
+        public async Task<IActionResult> AllUsers()
+        {
+            List<AllUsersViewModel> users = null;
+
+            try
+            {
+                users = await userService.GetAllUsers();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View(users);
+        }
+
+        public async Task<IActionResult> UserOrders(string id)
+        {
+            List<UserProductsViewModel> products = null;
+
+            
+            ApplicationUser user = await userService.GetUserById(id);
+
+            ViewData["Name"] = $"{user.FirstName} {user.LastName}";
+
+            try
+            {
+                products = await orderService.GetUserProducts(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View(products);
+        }
+    }
+}
