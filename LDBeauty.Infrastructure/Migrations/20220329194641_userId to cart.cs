56d36d10040b861316b0c4ebd3899b7dadcace32
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-03-31 19:44:06 +0300

    migation added userId to cart

---
+++ LDBeauty.Infrastructure/Migrations/20220329194641_userId to cart.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LDBeauty.Infrastructure.Migrations
+{
+    public partial class userIdtocart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Carts",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AspNetUsers_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AspNetUsers_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Carts");
+        }
+    }
+}
