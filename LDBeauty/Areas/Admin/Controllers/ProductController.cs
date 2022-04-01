cfc301cd68f71a051f70b3a9ab85c0932667b45a
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-04-01 23:27:56 +0300

    added try catch

--- LDBeauty/Areas/Admin/Controllers/ProductController.cs
+++ LDBeauty/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using LDBeauty.Core.Contracts;
 using LDBeauty.Core.Models.Cart;
 using LDBeauty.Core.Models.Product;
+using LDBeauty.Infrastructure.Data.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LDBeauty.Areas.Admin.Controllers
@@ -10,8 +11,7 @@ namespace LDBeauty.Areas.Admin.Controllers
     {
         private readonly IProductService productService;
 
-        public ProductController(
-            IProductService _productService)
+        public ProductController(IProductService _productService)
         {
             productService = _productService;
         }
@@ -24,6 +24,8 @@ namespace LDBeauty.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> AddProduct(AddProductViewModel model)
         {
+            ViewData[MessageConstant.SuccessMessage] = "Product was added successfuly";
+
             if (!ModelState.IsValid)
             {
                 ViewData[MessageConstant.ErrorMessage] = "Data is not correct!";
@@ -45,5 +47,47 @@ namespace LDBeauty.Areas.Admin.Controllers
             return View("/Admin/Product/AddProduct");
         }
 
+        public async Task<IActionResult> EditProduct(string id)
+        {
+
+            ProductDetailsViewModel product = null;
+
+            try
+            {
+                product = await productService.GetProduct(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProduct(AddProductViewModel model, string id)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                TempData[MessageConstant.ErrorMessage] = "Data is not correct!";
+                return View();
+            }
+
+            try
+            {
+                await productService.EditProduct(model, id);
+            }
+            catch (Exception)
+            {
+
+                TempData[MessageConstant.ErrorMessage] = "Something went wrong!";
+                return View();
+            }
+
+            ViewData[MessageConstant.SuccessMessage] = "Product was added successfuly";
+            return RedirectToAction();
+        }
     }
 }
