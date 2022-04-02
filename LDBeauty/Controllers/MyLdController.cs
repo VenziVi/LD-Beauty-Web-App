c18972065230b83655fa78cdbaf7d7337dfe9054
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-04-02 22:07:19 +0300

    add remove favourites actions

--- LDBeauty/Controllers/MyLdController.cs
+++ LDBeauty/Controllers/MyLdController.cs
@@ -1,4 +1,5 @@
 ﻿using LDBeauty.Core.Contracts;
+using LDBeauty.Core.Models.Gallery;
 using LDBeauty.Core.Models.Product;
 using LDBeauty.Core.Models.User;
 using LDBeauty.Core.Services;
@@ -14,15 +15,18 @@ namespace LDBeauty.Controllers
         private readonly IUserService userService;
         private readonly IOrderService orderService;
         private readonly IProductService productService;
+        private readonly IGalleryService galleryService;
 
         public MyLdController(
             IUserService _userService,
             IOrderService _orderService,
-            IProductService _productService)
+            IProductService _productService,
+            IGalleryService _galleryService)
         {
             userService = _userService;
             orderService = _orderService;
             productService = _productService;
+            galleryService = _galleryService;
         }
 
 
@@ -44,6 +48,54 @@ namespace LDBeauty.Controllers
             List<GetProductViewModel> products = await productService.GetFavouriteProducts(user);
 
             return View(products);
+        }
+
+        public async Task<IActionResult> RemoveProduct(string id)
+        {
+            var userName = User.Identity.Name;
+            ApplicationUser user = null;
+
+            try
+            {
+                user = await userService.GetUser(userName);
+                await productService.RemoveFromFavourite(id, user);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Redirect("/MyLd/FavouriteProducts");
+        }
+
+        public async Task<IActionResult> FavouriteImages()
+        {
+            var userName = User.Identity.Name;
+            ApplicationUser user = await userService.GetUser(userName);
+
+            IEnumerable<ImageViewModel> images = await galleryService.GetFavouriteImages(user);
+
+            return View(images);
+        }
+
+        public async Task<IActionResult> RemoveFromFavourites(int id)
+        {
+            var userName = User.Identity.Name;
+            ApplicationUser user = null;
+
+            try
+            {
+                user = await userService.GetUser(userName);
+                await galleryService.RemoveFromFavourite(id, user);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Redirect("/MyLd/FavouriteImages");
         }
     }
 }
