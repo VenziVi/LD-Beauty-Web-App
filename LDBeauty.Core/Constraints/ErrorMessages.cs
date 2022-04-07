a50ed7eeb369a03212d9edfd1a02ae22474aea72
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-04-07 21:57:25 +0300

    remove constraints folder

--- LDBeauty.Core/Constraints/ErrorMessages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LDBeauty.Core.Constraints
-{
-    public static class ErrorMessages
-    {
-        public const string DatabaseConnectionError = "There is a problem with database connection!";
-
-        public const string OutOfStockError = "You can't make an order becouse one or more products are out of stock!";
-    }
-}
