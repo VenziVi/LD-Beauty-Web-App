1f9bf55929c6ba7115901fd1be0a43706e12b342
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-04-07 21:57:09 +0300

    add helpers

---
+++ LDBeauty.Core/Helpers/CartProductDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Helpers
+{
+    public static class CartProductDelete
+    {
+        public static bool IsDeleted = false;
+    }
+}
