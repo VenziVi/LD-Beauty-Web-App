5048df8ebcddbcaeca7abe94f0c692b291465b26
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-04-12 23:23:20 +0300

    remove usings

--- LDBeauty.Core/Helpers/CartProductDelete.cs
+++ LDBeauty.Core/Helpers/CartProductDelete.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LDBeauty.Core.Helpers
+﻿namespace LDBeauty.Core.Helpers
 {
     public static class CartProductDelete
     {
